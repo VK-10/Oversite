b25bd8a571831ba5c66281e0ddfc754d0abee41a
light <visu2266@gmail.com>
2026-03-25 22:00:40 +0530

    backend in go

---
+++ backend/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("hello!")
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("PORT:", port)
+}
