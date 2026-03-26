52a254386317b7f11f233e721033320febc719ee
light <visu2266@gmail.com>
2026-03-26 14:40:09 +0530

    handlers mounting for http response and standard error

--- backend/go.mod
+++ backend/go.mod
@@ -1,5 +1,10 @@
-module github.com/VK-10/oversite
+module github.com/VK-10/oversite/backend
 
 go 1.24.0
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi/v5 v5.2.5
+	github.com/go-chi/cors v1.2.2
+)
--- backend/main.go
+++ backend/main.go
@@ -3,20 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello!")
 
-	godotenv.Load()
+	godotenv.Load(".env")
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
-
 	fmt.Println("PORT:", port)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/health", handlerReadiness)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on %v", srv.Addr)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
