2bfbd9dfd0ed4a71c95f4f9e6161d7a460fe4845
light <visu2266@gmail.com>
2026-04-13 18:26:07 +0530

    cleaned the description

--- django_server/api/views.py
+++ django_server/api/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.cache import cache_page
 from api.models import Scope
 from api.models import Tag
 import uuid
@@ -7,10 +8,26 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser 
 from api.models import News
-
+from bs4 import BeautifulSoup
 from rest_framework.decorators import api_view
 # Create your views here.
 
+def clean_post(post):
+    description = ""
+    if post.get("description"):
+        soup = BeautifulSoup(post["description"], "html.parser")
+        description = soup.get_text().strip()
+
+    return {
+        "id": post["id"],
+        "title": post["title"],
+        "description": description,
+        "published_at": post["published_at"],  # leave as is for now
+        "url": post["url"],
+        "feed": post["feed"],
+    }
+
+# @cache_page(60 * 10)
 @api_view(['GET'])
 def news(request):
         if request.method == 'GET':
@@ -23,7 +40,7 @@ def news(request):
                 city_name = request.query_params.get('subname')
                 locality = city_name
                 user = Tag.objects.get(name = name)
-                scope = Scope.objects.get(user_id = user.id, name = city_name)
+                scope = Scope.objects.get(user_id = user.id, name = locality)
                 post_list = News.objects.filter(feed_id = scope.id)
 
                 posts = ApiNewsSerializers(post_list, many = True)
@@ -31,9 +48,14 @@ def news(request):
             elif name == 'world-countries':
                 countries_name = request.query_params.get('subname')
                 locality = countries_name
-                usr_id = ""
+                user = Tag.objects.get(name = name)
+                scope = Scope.objects.get(user_id = user.id, name = locality)
+                post_list = News.objects.filter(feed_id = scope.id)
+
+                posts = ApiNewsSerializers(post_list, many = True)
                 key =""
 
-            print(posts)
-            return Response(posts.data)
+            cleaned = [clean_post(p) for p in posts.data]
+            # print(posts)
+            return Response(cleaned)
 
