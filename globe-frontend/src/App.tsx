b186fd2d2a031df68ff82c8b7cdf339a66d15ca3
light <visu2266@gmail.com>
2026-04-15 01:42:36 +0530

    router inside of a router

--- globe-frontend/src/App.tsx
+++ globe-frontend/src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import GlobeView from './pages/GlobeView'
 import LandingPage from './pages/LandingPage'
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/globe" element={<GlobeView />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
