550804377f63a09c7513abde67dbbcda649d37cc
light <visu2266@gmail.com>
2026-04-15 09:58:36 +0530

    rendered added on allowed hosts

--- django_server/config/settings.py
+++ django_server/config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['oversite-beryl.vercel.app/','127.0.0.1','localhost' ]
+ALLOWED_HOSTS = ['oversite-beryl.vercel.app/','127.0.0.1','localhost',"oversite.onrender.com", ]
 
 
 # Application definition
